8bebf54c3c674f56b472f418ae9cf7bc4b91c100
susumu <susumu@iorc.net>
2022-08-23 15:06:13 +0900

    aapiの仕様を決めãた

--- app/controllers/v1/general/login_controller.rb
+++ app/controllers/v1/general/login_controller.rb
@@ -2,7 +2,11 @@ class V1::General::LoginController < ApplicationController
     before_action :authenticate_v1_user!
 
     def index
-      render json: { message: 'hello' }
+      if !Base.exists?(user_id: current_user.id)
+        render json: { message: current_user.id }
+      else
+        render json: { message: 'yep'}
+      end
     end
   end
   
