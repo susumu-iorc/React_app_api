b8ef6382522fa77625574d2d16e247f1dc65e67a
susumu <susumu@iorc.net>
2022-08-24 17:33:32 +0900

    更新してレスポンスを返すようにした

--- app/controllers/v1/memo_controller.rb
+++ app/controllers/v1/memo_controller.rb
@@ -48,7 +48,7 @@ class V1::MemoController < ApplicationController
 
     # 指定されたメモが存在するかどうか
     if !Memo.exists?(place_id: post_body["place-id"], user_id: current_user.id)
-      render json: { "succes": false, "data": { "uid": current_user.id, "place-id": post_body["place-id"]}}
+      render json: { "success": false, "data": { "uid": current_user.id, "place-id": post_body["place-id"]}}
       return
     end
 
@@ -57,27 +57,55 @@ class V1::MemoController < ApplicationController
 
     # メモ更新の際のレスポンス
 
+    @memo_updt     = false
+    @memo_sucs     = false
+    @count_updt    = false
+    @count_sucs    = false
+    @favorite_updt = false
+    @favorite_sucs = false
+
     # memo の更新
     if !post_body["memo"].blank?
-      @memo.update(memo: post_body["memo"])
-
+      @memo_upd  = true
+      @memo.memo = post_body["memo"]
+      @memo_sucs = @memo.save
     end
     
     # count の更新
     if !post_body["count"].blank?
+      @count_updt = true
       if post_body["count"] == @memo.count + 1 || post_body["count"] == @memo.count - 1
-        @memo.update(count: post_body["count"])
-      else
+        @memo.count = post_body["count"]
+        @count_sucs = @memo.save
       end
     end
 
     # favorite の更新
     if !post_body["favorite"].blank?
-      if post_body["favorite"] >= 0 || post_body["favorite"] <= 3
-
-      @memo.update(favorite: post_body["memo"])
+      @favorite_updt = true
+      if post_body["favorite"] >= 0 && post_body["favorite"] <= 3
+        @memo.favorite = post_body["favorite"]
+        @favorite_sucs = @memo.save
       end
     end
+
+    render json: { "success": true, 
+                     "data": { 
+                                            "uid": current_user.id,
+                                       "place-id": post_body["place-id"],
+                                    "memo-update": @memo_updt,
+                                   "memo-success": @memo_sucs,
+                                   "count-update": @count_updt,
+                                  "count-success": @count_sucs,
+                                "favorite-update": @favorite_updt,
+                               "favorite-success": @favorite_sucs,
+                                       "contents": {
+                                                         "memo": @memo["memo"],
+                                                        "count": @memo["count"],
+                                                     "favorite": @memo["favorite"]
+                                                    }
+                              }
+                  }
   end
 end
 
