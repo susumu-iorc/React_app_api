2addb4c47081a3fdd32ba266b5493d95c677f14a
susumu <susumu@iorc.net>
2022-09-21 12:05:31 +0900

    cors tabememo.iorc.net

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "*" 
+      origins "https://tabememo.iorc.net" 
       resource "*",
         headers: :any,
         expose: ["access-token", "expiry", "token-type", "uid", "client"], 
